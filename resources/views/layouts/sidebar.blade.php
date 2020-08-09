3dea55bce53f62594edfcf9818db08e52d1cacdc
danndisetiaaji260 <dandi.setiaaji@gmail.com>
2020-08-09 03:43:09 -0700

    pert 13

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,7 @@
 <h4> Main Menu</h4>
 <div class="list-group list-group-flush">
   <a href="/mhs" class="list-group-item list-group-item-action">Mahasiswa</a>
+  <a href="#" class="list-group-item list-group-item-action">Dosen</a>
   <a href="/prodi" class="list-group-item list-group-item-action">Prodi</a>
   
 </div>
